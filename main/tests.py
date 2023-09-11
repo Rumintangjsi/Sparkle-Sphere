c9e18964a2af585d7db210c18ef5ba729a0484ba
Rumintangjsi <rumintanghtgl@gmail.com>
2023-09-11 15:43:20 +0700

    Update test dan view

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,15 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+    # Check untuk bagian show_main pada view, dan memeriksa response
+    def check_app_detail (self):
+        # Mengirimkan request ke view show_main
+        response = Client().get('/main/')
+        
+        # Menggecek apakah response status code adalah 200
+        self.assertEquals(response.status_code, 200)
+
+        self.assertEquals(response.context['app'], 'Sparkle Sphere')
+        self.assertEquals(response.context['name'], 'Rumintang Jessica H')
+        self.assertEquals(response.context['class'], 'PBP A')
--- main/views.py
+++ main/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
+
 def show_main(request):
     context = {
         'name': 'Rumintang Jessica H',
+        'app' : 'Sparkle Sphere',
         'class': 'PBP A'
     }
 
     return render(request, "main.html", context)
-
-# Create your views here.
